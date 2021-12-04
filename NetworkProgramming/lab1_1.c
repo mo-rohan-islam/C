c0886048a10547294457c4010e4f65c389d9daff
nagrohan726 <nag.rohan726@gmail.com>
2021-12-04 18:25:04 +0530

    Incomplete programs

--- NetworkProgramming/lab1_1.c
+++ NetworkProgramming/lab1_1.c
@@ -9,17 +9,17 @@ int main()
     pid = fork(); //create another process
     if (pid < 0)
     { //fail
-        printf(“\nFork failed\n”);
+        printf("\nFork failed\n");
         exit(-1);
     }
     else if (pid == 0)
-    {                                             //child
-        execlp( “/ bin / ls”, “ls”, “- l”, NULL); //execute ls
+    {                                        //child
+        execlp("/bin/ls”, “ls”, “-l", NULL); //execute ls
     }
     else
     {               //parent
         wait(NULL); //wait for child
-        printf(“\nchild complete\n”);
+        printf("\nchild complete\n");
         exit(0);
     }
 }
